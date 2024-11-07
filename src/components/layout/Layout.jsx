e47f750720d56bfc84518c60d024fd4fdf3119db
nima <alimahmoodi1706.6@gmail.com>
2024-11-07 09:50:32 +0330

    add some animation to about us page's image and add an scroll bar in top

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,10 +3,12 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Top from "./Top";
 import "./layout.css";
+import ScrollBar from "./ScrollBar";
 
 function Layout({ children }) {
   return (
     <>
+      <ScrollBar />
       <Header />
       <div className="layout__children">{children}</div>
       <Top />
---
+++ src/components/layout/ScrollBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./scrollBar.module.css"
+
+function ScrollBar() {
+  return (
+    <div className={styles.scrollBar}></div>
+  )
+}
+
+export default ScrollBar
