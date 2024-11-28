13b2abfd1b46c4676edafca8e938ebacaea7be00
nima <alimahmoodi1706.6@gmail.com>
2024-11-28 08:11:00 +0330

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,27 @@ import CartProvider from "services/CartProvider";
 import UserProvider from "services/UserProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import Spinner from "components/spinner/Spinner";
 
 // Create a new QueryClient instance
 const queryClient = new QueryClient();
 
 function App() {
+  let [show, setShow] = useState(false);
+  window.addEventListener("load", () => setShow(true));
   return (
-    <QueryClientProvider client={queryClient}> 
+    <QueryClientProvider client={queryClient}>
       <CartProvider>
         <UserProvider>
           <BrowserRouter>
-            <Layout>
-              <Router />
-            </Layout>
+            {show ? (
+              <Layout>
+                <Router />
+              </Layout>
+            ) : (
+              <Spinner />
+            )}
           </BrowserRouter>
         </UserProvider>
       </CartProvider>
